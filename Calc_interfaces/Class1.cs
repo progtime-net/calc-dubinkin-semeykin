dd8147aaa5648a6eda328153e0f07cc5a5277851
AnloGames <7383an@gmail.com>
2023-08-11 14:22:45 +0300

    init

---
+++ Calc_interfaces/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_interfaces
+{
+    public class Step
+    {
+        public int Obj { get; set; }
+        public string Operation { get; set; }
+    }
+}
