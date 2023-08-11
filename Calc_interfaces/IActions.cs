d281c5f7b207f84094b040f42c01698fcec7a636
Mootfrost777 <andrey13102007@gmail.com>
2023-08-11 14:34:49 +0300

    Fix IActions

--- Calc_interfaces/IActions.cs
+++ Calc_interfaces/IActions.cs
@@ -2,5 +2,11 @@
 
 public interface IActions
 {
+    public double Sum(double n1, double n2);
     
+    public double Subtract(double n1, double n2);
+    
+    public double Multiply(double n1, double n2);
+    
+    public double Divide(double n1, double n2);
 }
