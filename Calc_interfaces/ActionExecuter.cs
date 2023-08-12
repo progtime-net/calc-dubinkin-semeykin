abe41c37babb52f953820506d3ed5d62f64fb3cb
Mootfrost777 <andrey13102007@gmail.com>
2023-08-12 13:28:45 +0300

    Add ActionsExecuter

---
+++ Calc_interfaces/ActionExecuter.cs
@@ -0,0 +1,34 @@
+﻿namespace Calc_interfaces;
+
+public class ActionExecuter : IActionExecuter
+{
+    private Sum sum = new Sum();
+    private Subtract sub = new Subtract();
+    private Multiplication mul = new Multiplication();
+    private Divsion div = new Divsion();
+    
+    public double PerformActions(List<Step> steps)
+    {
+        double result = 0;
+        foreach (var step in steps)
+        {
+            switch (step.Operation)
+            {
+                case "+":
+                    sum.Operation(result, step.Obj);
+                    break;
+                case "-":
+                    sub.Operation(result, step.Obj);
+                    break;
+                case "*":
+                    mul.Operation(result, step.Obj);
+                    break;
+                case "/":
+                    div.Operation(result, step.Obj);
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
