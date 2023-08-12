9a5d62beeed4032cfd57b4b1ea755cb8ba5c6e43
Kaktus200020 <artemkremnev2007@gmail.com>
2023-08-12 13:25:26 +0300

    addActionsClasses

---
+++ Calc_interfaces/Actions/Multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_interfaces.Actions
+{
+    public class Multiplication : IAction
+    {
+        public double Operation(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
